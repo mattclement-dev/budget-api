e7202657ab2195acb85e5e96ed9a2a4bfc23841d
Yusuf <101431451+Dentistkiller@users.noreply.github.com>
2025-08-08 08:51:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/goals", goalRoutes);
 app.use("/api/summary", summaryRoutes); 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
